f3e3b618f18c81bbf6c92e18cd19fe22107b1a0f
Xiaolei Wang <xwang@sandiego.edu>
2019-12-24 20:35:56 -0800

    added sub-header text to landing page

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -8,27 +8,28 @@ class Landing extends Component {
             <main className="landing-page" role="main">
                 <header className="title-header" role="banner">
                     <h1 className="title-name">Flavor Logs</h1>
+                    <h2 className="title-sub">A record of dishes you've eaten at your favorite places</h2>
                 </header>
 
                 <section>
-                    <header>
-                        <h2>Getting Started</h2>
+                    <header >
+                        <h3 className="landing-sub-header">Getting Started</h3>
                     </header>
                     <p>Flavor Log helps you remember the best dishes (or the not-so-best dishes) from new eateries as well as ones you like to frequent.</p>
                     <p>[<em>Screenshot placeholder of Logs</em>]</p>
                 </section>
 
                 <section>
-                    <header>
-                        <h2>Add a Eatery</h2>
+                    <header >
+                        <h3 className="landing-sub-header">Add a Eatery</h3>
                     </header>
                     <p>Begin your log by adding a eatery, such as a new restaurant you visited, or an old bakery with your favorite desserts.</p>
                     <p>[<em>Screenshot placeholder of Add Eatery Form</em>]</p>
                 </section>
 
                 <section>
-                    <header>
-                        <h2>Add a Log</h2>
+                    <header >
+                        <h3 className="landing-sub-header">Add a Log</h3>
                     </header>
                     <p>Here, you can note details about the dish you had at a eatery, give it a rating, and the last date you ate it.</p>
                     <p>[<em>Screenshot placeholder of Add Log Form</em>]</p>
