e73c4d6c4979f85918c34fcac4b75d1cc6286c13
Xiaolei Wang <xwang@sandiego.edu>
2020-01-14 21:43:25 -0800

    began responsive CSS editing

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -15,14 +15,14 @@ class Landing extends Component {
                     <header >
                         <h3 className="landing-sub-header">Getting Started</h3>
                     </header>
-                    <p>Flavor Log helps you remember the best dishes (or the not-so-best dishes) from new eateries as well as ones you like to frequent.</p>
+                    <p className="landing-info">Flavor Log helps you remember the best dishes (or the not-so-best dishes) from new eateries as well as ones you like to frequent.</p>
                     <img className="my-logs-gif" src="https://i.imgur.com/IzsBWw1.gif" alt="Scrolling gif of My Logs Page"></img>
                 </section>
                 <section>
                     <header >
                         <h3 className="landing-sub-header">Add an Eatery</h3>
                     </header>
-                    <p>Begin your log by adding a eatery, such as a new restaurant you visited, or an old bakery with your favorite desserts.</p>
+                    <p className="landing-info">Begin your log by adding a eatery, such as a new restaurant you visited, or an old bakery with your favorite desserts.</p>
                     <img className="add-eatery-gif" src="https://i.imgur.com/PhqgmuS.gif" alt="Scrolling gif of Add Eatery Form"></img>
                 </section>
 
@@ -30,7 +30,7 @@ class Landing extends Component {
                     <header >
                         <h3 className="landing-sub-header">Add a Log</h3>
                     </header>
-                    <p>Here, you can note details about the dish you had at a eatery, give it a rating, and the last date you ate it.</p>
+                    <p className="landing-info">Here, you can note details about the dish you had at a eatery, give it a rating, and the last date you ate it.</p>
                     <img className="add-log-gif" src="https://i.imgur.com/l4rs3WV.gif" alt="Scrolling gif of Add Log Form"></img>
                 </section>
 
